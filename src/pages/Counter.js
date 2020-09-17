bb61c6f267b326b20fe74edfc05652b7f9b6b31a
Nathaniel Friedman <nathaniel.friedman@duke.edu>
2020-09-17 01:22:57 -0400

    first

--- src/pages/Counter.js
+++ src/pages/Counter.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import axios from 'axios';
 
-const url = "http://localhost:5000";
+const url = "https://felix-jiang-backend.herokuapp.com";
 class Counter extends React.Component {
 
     state = {
-        count: 0
+        count: 0,
     }
 
-    update = () => {
-        axios.get(url + '/count', (res, err) => {
-            console.log(res);
-        })
+    async componentDidMount() {
+        const res = await axios.get(url + '/count')
+            .then(res => {
+                console.log(res);
+                return res.data;
+            })
+            .catch(err => {
+                return this.state.count;
+            });
+        this.setState({count: res});
+    }
+
+    update = async () => {
+        await axios.post(url + '/count')
+            .then(res => {
+                console.log(res.data);
+            })
+            .catch(err => {
+
+            });
         this.setState({count: this.state.count + 1});
     }
 
